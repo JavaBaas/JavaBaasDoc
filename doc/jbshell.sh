b37567091ac6873d5278227e980159e69dbfa7d7
zangyilin <112408136qq@gmail.com>
2017-11-02 16:51:26 +0800

    更新安装说明

--- doc/jbshell.sh
+++ doc/jbshell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "begin install"
+echo "开始安装"
 
 JAVABAAS_SERVICE="http://7xr649.dl1.z0.glb.clouddn.com/jbshell.zip"
 
@@ -18,58 +18,58 @@ EOF
 )
 
 
-echo "Looking for a previous installation of javabaasshell..."
+echo "检查是否已经安装jbshell..."
 if [ -d "$JBSHELL_DIR" ]; then
-	echo "javabaasshell found."
+	echo "发现jbshell"
 	echo ""
 	echo "======================================================================================================"
-	echo " You already have javabaasshell installed."
-	echo " javabaasshell was found at:"
+	echo "你已经安装了jbshell "
+	echo " 地址为:"
 	echo ""
 	echo "    ${JBSHELL_DIR}"
 	echo ""
-	echo " Please consider running the following if you need to upgrade."
-	echo ""
-	echo "    $ jbshell selfupdate force"
+	echo "请检查"
+	echo " 如果你想更新jbshell 请使用"
+	echo "    $ jb u    或   $ jb update"
 	echo ""
 	echo "======================================================================================================"
 	echo ""
 	exit 0
 fi
 
-echo "Looking for unzip..."
+echo "检查是否安装了unzip..."
 if [ -z $(which unzip) ]; then
-	echo "Not found."
+	echo "没有发现unzip."
 	echo "======================================================================================================"
-	echo " Please install unzip on your system using your favourite package manager."
+	echo " 请安装unzip."
 	echo ""
-	echo " Restart after installing unzip."
+	echo " 安装完unzip后请重新安装jbshell."
 	echo "======================================================================================================"
 	echo ""
 	exit 0
 fi
 
-echo "Looking for curl..."
+echo "检查是否安装了curl..."
 if [ -z $(which curl) ]; then
-	echo "Not found."
+	echo "没有发现curl."
 	echo ""
 	echo "======================================================================================================"
-	echo " Please install curl on your system using your favourite package manager."
+	echo " 请安装curl."
 	echo ""
-	echo " Restart after installing curl."
+	echo " 安装完curl后请重新安装jbshell."
 	echo "======================================================================================================"
 	echo ""
 	exit 0
 fi
 
-echo "Looking for java..."
+echo "检查是否安装了java..."
 if [ -z $(which java) ]; then
-	echo "Not found."
+	echo "没有发现java."
 	echo ""
 	echo "======================================================================================================"
-	echo " Please install java(1.8+) on your system using your favourite package manager."
+	echo " 请安装java（最低版本1.7）."
 	echo ""
-	echo " Restart after installing curl."
+	echo " 安装完java后请重新安装jbshell."
 	echo "======================================================================================================"
 	echo ""
 	exit 0
@@ -77,47 +77,47 @@ fi
 
 jbshell_zip_file="${JAVABAAS_DIR}/javabaasshell.zip"
 
-echo "create distribution directories..."
+echo "创建jbshell目录..."
 mkdir -p "$JAVABAAS_DIR"
-echo "create dir success..."
+echo "创建jbshell目录成功..."
 curl -L "$JAVABAAS_SERVICE" > "$jbshell_zip_file"
-echo "download file success..."
+echo "下载jbshell..."
 unzip -qo "$jbshell_zip_file" -d "$JAVABAAS_DIR" -x __MACOSX/*
 rm -f "$jbshell_zip_file"
-echo "unzip zip success"
+echo "解压成功"
 
-echo "Attempt update of bash profiles..."
+echo "添加bash环境变量..."
 if [ ! -f "$javabaas_bashrc" ]; then
 	echo "#!/bin/bash" > "$javabaas_bashrc"
 	echo "$javabaas_init_snippet" >> "$javabaas_bashrc"
-	echo "Created and initialised ${javabaas_bashrc}"
+	echo "创建并更新 ${javabaas_bashrc}"
 	#source "$javabaas_bashrc"
 else
 	if [[ -z $(grep 'JBSHELL_DIR' "$javabaas_bashrc") ]]; then
 		echo -e "\n$javabaas_init_snippet" >> "$javabaas_bashrc"
-		echo "Updated existing ${javabaas_bashrc}"
+		echo "更新 ${javabaas_bashrc}"
 		#source "$javabaas_bashrc"
 	fi
 fi
 
-echo "Attempt update of zsh profiles..."
+echo "添加zsh环境变量..."
 if [ ! -f "$javabaas_zhsrc" ]; then
 	echo "$javabaas_init_snippet" >> "$javabaas_zhsrc"
-	echo "Created and initialised ${javabaas_zhsrc}"
+	echo "创建并更新 ${javabaas_zhsrc}"
 	#source "$javabaas_zhsrc"
 else
 	if [[ -z $(grep 'JBSHELL_DIR' "$javabaas_zhsrc") ]]; then
 		echo -e "\n$javabaas_init_snippet" >> "$javabaas_zhsrc"
-		echo "Updated existing ${javabaas_zhsrc}"
+		echo "更新 ${javabaas_zhsrc}"
 		#source "$javabaas_zhsrc"
 	fi
 fi
 
 
-echo "Success install javabaasshell"
-echo "Please open a new terminal, or run the following in the existing one:"
+echo "成功安装jbshell"
+echo "请打开一个新的终端，或者在当前终端执行下面命令:"
 echo ""
 echo "    source \"${JAVABAAS_DIR}/jbshell/export/jbexport\""
 echo ""
 echo ""
-echo "Enjoy!!!"
+echo "开始造吧!!!"
